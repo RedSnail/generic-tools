f2c2d998fd32507b046768c4733d9d5288bcf3de
RedSnail <Oleg.Demianchenko@gmail.com>
2018-06-25 22:42:09 +0300

    the test now is okay

--- src/test/java/GenericTypeManagerTest.java
+++ src/test/java/GenericTypeManagerTest.java
@@ -1,12 +1,11 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class GenericTypeManagerTest {
     @Test public void testGetGenericTypeClass() {
         GenericTypeManager<String> manager = new GenericTypeManager<>();
-        assertNotNull("test GenericTypeManager's ability to get Class<String> obj from String as a generic type",
+        assertNull("test GenericTypeManager's ability to get Class<String> obj from String as a generic type",
                 manager.getGenericTypeClass());
     }
 }
