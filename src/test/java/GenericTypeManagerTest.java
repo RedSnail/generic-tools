cb04f9f5cf1cb4f1c247e2ce75a4cb05b16c8e33
RedSnail <Oleg.Demianchenko@gmail.com>
2018-06-25 22:38:42 +0300

    wanna fail a test

--- src/test/java/GenericTypeManagerTest.java
+++ src/test/java/GenericTypeManagerTest.java
@@ -1,11 +1,12 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class GenericTypeManagerTest {
     @Test public void testGetGenericTypeClass() {
         GenericTypeManager<String> manager = new GenericTypeManager<>();
-        assertNull("test GenericTypeManager's ability to get Class<String> obj from String as a generic type",
+        assertNotNull("test GenericTypeManager's ability to get Class<String> obj from String as a generic type",
                 manager.getGenericTypeClass());
     }
 }
